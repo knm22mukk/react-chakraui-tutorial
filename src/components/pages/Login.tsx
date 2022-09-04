e28cb5f7afeb4be5ee945d577ead3710cb262abe
knm22mukk <knm22mukk@gmail.com>
2022-09-04 15:54:43 +0900

    ログインボタンを共通コンポーネント化しました。

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Divider, Flex, Heading, Input, Stack } from '@chakra-ui/react';
+import { Box, Divider, Flex, Heading, Input, Stack } from '@chakra-ui/react';
 import { FC } from 'react';
+import { PrimaryButton } from '../atoms/button/PrimaryButton';
 
 export const Login: FC = () => {
   return (
@@ -11,9 +12,7 @@ export const Login: FC = () => {
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder='ユーザーID' />
-          <Button bg='teal.400' color='white' _hover={{ opacity: '0.8' }}>
-            ログイン
-          </Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
