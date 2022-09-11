e6e4817e592c51a890abca632248a747333d5aad
knm22mukk <knm22mukk@gmail.com>
2022-09-11 21:31:03 +0900

    ユーザー詳細モーダル画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,33 @@
-import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
-import { FC, useEffect } from 'react';
+import {
+  Center,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  Stack,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
+import { FC, useCallback, useEffect } from 'react';
 import { useAllUsers } from '../../hooks/useAllUsers';
 import { UserCard } from '../organisms/layout/user/UserCard';
+import { UserDetailModal } from '../organisms/layout/user/UserDetailModal';
 import { HeaderLayout } from '../template/HeaderLayout';
 
 export const UserManagement: FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
 
   useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <HeaderLayout>
@@ -19,11 +39,17 @@ export const UserManagement: FC = () => {
         <Wrap p={{ base: 4, md: 10 }}>
           {users.map((user) => (
             <WrapItem key={user.id} mx='auto'>
-              <UserCard imageUrl='/logo192.png' userName={user.username} fullName={user.name} />
+              <UserCard
+                imageUrl='/logo192.png'
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </HeaderLayout>
   );
 };
